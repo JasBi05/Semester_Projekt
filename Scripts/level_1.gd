f58b0244a5a6726b55ab0e743002718d68a7cd59
JilVM <157635701+JilVM@users.noreply.github.com>
2024-02-09 19:31:16 +0100

    still broken + added assets

--- Scripts/level_1.gd
+++ Scripts/level_1.gd
@@ -1,6 +1,5 @@
 extends Node2D
 
-
 func _ready():
 	var playerscene = load("res://scences/player.tscn")
 	var player = playerscene.instantiate()
--- Scripts/world.gd
+++ Scripts/world.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
 func _ready():
-		var level_1scene = load("res://Scences/level_1.tscn")
-		var level_1 = level_1scene.instantiate()
-		add_child(level_1)
+	var level_1scene = load("res://scences/level_1.tscn")
+	var level_1 = level_1scene.instantiate()
+	add_child(level_1)
