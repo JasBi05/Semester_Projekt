530dc863aac0c4e4894e7afb428906690c4f4389
JasBi05 <jassi300805@gmail.com>
2024-02-23 11:28:42 +0100

    added audio to level 1

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -5,6 +5,7 @@ const SPEED 		= 100
 const JUMP_POWER 	= 200
 const UP_VECTOR 	= Vector2(0,-1)
 
+
 func _ready():
 	pass 
 
@@ -21,13 +22,17 @@ func _process(delta):
 	
 	move_and_slide()
 	
+	
 
+   
 func check_key_input():
 	if Input.is_action_pressed("left"):
 		velocity.x = -SPEED
-	
+		
+				
 	if Input.is_action_pressed("right"):
 		velocity.x = SPEED
-	
+		
+			
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = -JUMP_POWER
