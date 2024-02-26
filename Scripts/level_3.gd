4b9f317f573f7f2ddd4b9cb99fff2f8fe2089627
JasBi05 <jassi300805@gmail.com>
2024-02-26 14:39:26 +0100

    added mirror

---
+++ Scripts/level_3.gd
@@ -0,0 +1,15 @@
+extends Node2D
+var bg_music := AudioStreamPlayer.new()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	# --- spawn Player ---
+	#var playerscene = load("res://scences/player.tscn")
+	#var player = playerscene.instantiate()
+	#add_child(player)
+	# ------
+	bg_music.stream = load("res://assets/audio/Pack/3- Something's wrong.mp3")
+	bg_music.autoplay = true
+	add_child(bg_music)
+	
+
