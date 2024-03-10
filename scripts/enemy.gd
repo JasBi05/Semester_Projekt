8a4ce5aa1044f4dbdb7748e65da484e04056e9eb
JilVM <157635701+JilVM@users.noreply.github.com>
2024-03-11 00:23:04 +0100

    Enemy is working now (different than it should be, but it works)

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,17 +1,43 @@
 extends CharacterBody2D
 
-var speed = 50
-var player_position
-var target_position
-var leben = 1
+const SPEED_E = 50
+var active: bool = false
+@onready var nav_agent: NavigationAgent2D = $NavigationAgent2D
 
-@onready var player = get_parent().get_node("player")
 
-func _physics_process(_delta):
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+			nav_agent.target_position = get_global_mouse_position()
+			active = true
+
+
+func _physics_process(_delta: float) -> void:
+	if active:
+		var next_path_pos := nav_agent.get_next_path_position()
+		var direction := global_position.direction_to(next_path_pos)
+		velocity = direction * SPEED_E
+	else:
+		velocity = Vector2.ZERO
 	
-	player_position = player.position
-	target_position = (player_position - position).normalized()
-	
-	if position.distance_to(player_position) > 0:
-		velocity = target_position * speed
-		move_and_slide()
+	move_and_slide()
+
+
+func enable_npc() -> void:
+	print("NPC activated")
+	nav_agent.target_position = $"../CharacterBody2D".global_position
+	active = true
+
+func disable_npc() -> void:
+	print("NPC Disabled\n")
+	active = false
+
+func _on_nav_agent_2d_target_reached() -> void:
+	print("Reached target")
+	disable_npc()
+
+
+func _on_nav_agent_2d_navigation_finished() -> void:
+	print("Navigation finished")
+	disable_npc()
+
